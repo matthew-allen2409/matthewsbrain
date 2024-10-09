e3e08736fc9ef9699970450b8f0aebfba843fd8d
matthew-allen2409 <matthew.allen2409@gmail.com>
2024-10-09 00:06:12 -0500

    using environment variables

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 use blog_controller::initializer;
 use blog_controller::initializer::database::DBConfig;
+use dotenv::dotenv;
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     let db_config = DBConfig {
-        max_connections: 50,
-        user: String::from("app"),
-        password: String::from("xauY5.B;<E*X~+y%"),
-        host: String::from("localhost"),
-        database: String::from("blog"),
+        max_connections: env::var("MAX_CONNECTIONS")
+            .expect("MAX_CONNECTIONS must be set")
+            .parse()
+            .unwrap(),
+        user: env::var("DB_USER").expect("DB_USER must be set"),
+        password: env::var("DB_PASSWORD").expect("DB_PASSWORD must be set"),
+        host: env::var("DB_HOST").expect("DB_HOST must be set"),
+        database: env::var("DB_NAME").expect("DB_NAME must be set"),
     };
 
-    let host = "localhost";
-    let port = "8080";
-    let address = format!("{host}:{port}");
+    let address = env::var("ADDRESS").expect("ADDRESS must be set");
     let config = initializer::ApplicationConfig { address, db_config };
 
     initializer::initialize(config).await;
